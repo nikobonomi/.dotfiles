b8513db72cb49b4d4a1d7a00d44a9d076c98ba59
Niko <9111900+nikobonomi@users.noreply.github.com>
2024-08-16 17:57:21 +0200

    chore: crlf to lf

--- zsh/os/mac.zsh
+++ zsh/os/mac.zsh
@@ -2,3 +2,9 @@ alias ll="colorls -A"
 alias ls="colorls"
 alias lsa="colorls -lA --sd"
 alias chrome="open -a \"Google Chrome\""
+
+dos2mac() {
+    tr -d '\015' < "$1" > "tmp.$1"
+    mv "tmp.$1" "$1"
+}
+
