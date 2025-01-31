1b29f643eb390d176b455acade0e07503f00a1a1
Niko Bonomi <niko.bonomi@post.ch>
2025-01-31 12:15:21 +0100

    delay train fix

--- nvim/after/plugin/delay-train.lua
+++ nvim/after/plugin/delay-train.lua
@@ -1,4 +1,8 @@
 require('delaytrain').setup {
     grace_period = 3,
-    ['nv'] = { 'h', 'j', 'k', 'l', 'e', 'b' }
+    keys = { -- Which keys (in which modes) should be delayed
+        ['nv'] = { 'h', 'j', 'k', 'l', 'e', 'b' }
+    },
+    ignore_filetypes = { "help", "NvimTr*" }
+
 }
